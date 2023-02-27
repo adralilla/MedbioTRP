c8b1abbe338c5fa2f700783c0eb976eb02b30697
Adravecz Lilla <adralilla@gmail.com>
2023-02-27 22:40:04 +0100

    small changes to finetuning

--- finetuning.py
+++ finetuning.py
@@ -1,7 +1,14 @@
 import random
 import torch
-from transformers import Trainer, TrainingArguments
+from transformers import (
+    Trainer,
+    TrainingArguments,
+    AutoModelForSequenceClassification,
+    AutoConfig,
+    AutoTokenizer,
+)
 import pandas as pd
+from datasets import Dataset
 
 training_set_bact = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/IDS_InheritDBTraining_LabelBac_k6_lcashift1_Ls512"
 training_set_phage = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/IDS_InheritDBTraining_LabelPha_k6_lcashift1_Ls512"
@@ -9,13 +16,16 @@ training_set_phage = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/I
 eval_set_bact = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/IDS_InheritDBValidation_LabelBac_k6_lcashift1_Ls512"
 eval_set_phage = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/IDS_InheritDBValidation_LabelPha_k6_lcashift1_Ls512"
 
-model = "./models/checkpoint-61500"
+model_path = "./models/checkpoint-61500/"
 n = 10000
 k = 1000
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 print()
+
+config = AutoConfig.from_pretrained(f"{model_path}config.json")
+model = AutoModelForSequenceClassification.from_config(config)
 
 with open(training_set_bact) as f:
     records_train_bact = f.readlines()
@@ -34,22 +44,27 @@ with open(eval_set_bact) as f:
 records_eval_bact = pd.DataFrame(
     {"sentences": random.sample(records_eval_bact, n), "label": 0}
 )
-
 with open(eval_set_phage) as f:
     records_eval_phage = f.readlines()
 records_eval_phage = pd.DataFrame(
     {"sentences": random.sample(records_eval_phage, n), "label": 1}
 )
 
-train_dataset = pd.concat([records_train_bact, records_train_phage], ignore_index=True)
-eval_dataset = pd.concat([records_eval_bact, records_eval_phage], ignore_index=True)
+train_dataset = Dataset.from_pandas(
+    pd.concat([records_train_bact, records_train_phage], ignore_index=True)
+)
+eval_dataset = Dataset.from_pandas(
+    pd.concat([records_eval_bact, records_eval_phage], ignore_index=True)
+)
 training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
+    tokenizer=tokenizer,
 )
 
 trainer.train()
