7aae9a4023285d18e1359a66eab4a51621ed8ae1
Adravecz Lilla <adralilla@gmail.com>
2023-02-27 16:41:20 +0100

    updated finetuning

--- finetuning.py
+++ finetuning.py
@@ -1,57 +1,55 @@
+import random
 import torch
-from transformers import (
-    AutoTokenizer,
-    AutoConfig,
-    AutoModelForSequenceClassification,
-    TrainingArguments,
-    Trainer,
-    DataCollatorWithPadding,
+from transformers import Trainer, TrainingArguments
+import pandas as pd
+
+training_set_bact = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/IDS_InheritDBTraining_LabelBac_k6_lcashift1_Ls512"
+training_set_phage = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/IDS_InheritDBTraining_LabelPha_k6_lcashift1_Ls512"
+
+eval_set_bact = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/IDS_InheritDBValidation_LabelBac_k6_lcashift1_Ls512"
+eval_set_phage = "/scratch/fastscratch/NBL/training_datasets/inherit_phage/IDS_InheritDBValidation_LabelPha_k6_lcashift1_Ls512"
+
+model = "./models/checkpoint-61500"
+n = 10000
+k = 1000
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+print()
+
+with open(training_set_bact) as f:
+    records_train_bact = f.readlines()
+records_train_bact = pd.DataFrame(
+    {"sentences": random.sample(records_train_bact, n), "label": 0}
 )
-from datasets import load_dataset
-import numpy as np
-import evaluate
 
-data_files = {
-    "train": "train_dataset.csv",
-    # "test": "test_dataset.csv",
-    # "validation": "validation_dataset.csv",
-}
-raw_dataset = load_dataset("csv", data_files=data_files)
-tokenizer = AutoTokenizer.from_pretrained("./models/checkpoint-61500/")
+with open(training_set_phage) as f:
+    records_train_phage = f.readlines()
+records_train_phage = pd.DataFrame(
+    {"sentences": random.sample(records_train_phage, n), "label": 1}
+)
 
+with open(eval_set_bact) as f:
+    records_eval_bact = f.readlines()
+records_eval_bact = pd.DataFrame(
+    {"sentences": random.sample(records_eval_bact, n), "label": 0}
+)
 
-def tokenize_function(example):
-    return tokenizer(
-        example["sequence"],
-        padding=True,
-        return_tensors="pt",
-        is_split_into_words=True,
-        truncation=True,
-        model_max_length=512,
-    )
+with open(eval_set_phage) as f:
+    records_eval_phage = f.readlines()
+records_eval_phage = pd.DataFrame(
+    {"sentences": random.sample(records_eval_phage, n), "label": 1}
+)
 
-
-tokenized_datasets = raw_dataset.map(tokenize_function, batched=True)
-
-
-# def compute_metrics(eval_preds):
-#     metric = evaluate.load(" ")
-#     logits, labels = eval_preds
-#     predictions = np.argmax(logits, axis=-1)
-#     return metric.compute(predictions=predictions, references=labels)
-
-
-config = AutoConfig.from_json_file("./models/checkpoint-61500/config.json")
-model = AutoModelForSequenceClassification.from_config(config)
-# training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+train_dataset = pd.concat([records_train_bact, records_train_phage], ignore_index=True)
+eval_dataset = pd.concat([records_eval_bact, records_eval_phage], ignore_index=True)
+training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
 
 trainer = Trainer(
-    model,
-    # training_args,
-    train_dataset=tokenized_datasets["train"],
-    # eval_dataset=tokenized_datasets["validation"],
-    data_collator=data_collator,
-    tokenizer=tokenizer,
-    # compute_metrics=compute_metrics,
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
 )
+
+trainer.train()
